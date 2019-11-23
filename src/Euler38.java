7ab67c24bc6f2b1f07e4945fedb3baca9e965df5
nikolag <nikola.georgiev2000@hotmail.com>
2019-11-23 01:34:25 +0000

    Completed and cleaned up

--- src/Euler38.java
+++ src/Euler38.java
@@ -1,8 +1,5 @@
 //https://projecteuler.net/problem=32
 
-import java.util.ArrayList;
-
-
 public class Euler38 {
 
     public static void main(String[] args) {
@@ -15,13 +12,13 @@ public class Euler38 {
         int max = 0;
         for (int i = 1; i < Math.pow(10, 5); i++) {
             concat = "";
-            for (int j = 1; j < 9/(Math.log10(i)+1)+1; j++) {
-                concat+=i*j;
+            for (int j = 1; j < 9 / (Math.log10(i) + 1) + 1; j++) {
+                concat += i * j;
             }
             if (EulerToolkit.panDigitals(concat)) {
-                if(Integer.parseInt(concat)>max){
+                if (Integer.parseInt(concat) > max) {
                     max = Integer.parseInt(concat);
-                };
+                }
             }
         }
 
--- src/Euler40.java
+++ src/Euler40.java
@@ -3,22 +3,22 @@ public class Euler40 {
     public static void main(String[] args) {
         int total = 1;
         for (int i = 0; i < 6; i++) {
-            total*=chap((int) Math.pow(10,i)-1);
+            total *= chap((int) Math.pow(10, i) - 1);
         }
         System.out.println(total);
     }
 
-    public static int chap(int n){
+    public static int chap(int n) {
         int degree = 0;
         int position;
         WHILE:
-        while(true){
-            for (int i = (int) (Math.pow(10,degree)); i < Math.pow(10,degree+1); i++) {
-                if(n<=degree){
+        while (true) {
+            for (int i = (int) (Math.pow(10, degree)); i < Math.pow(10, degree + 1); i++) {
+                if (n <= degree) {
                     position = i;
                     break WHILE;
                 }
-                n-=(degree+1);
+                n -= (degree + 1);
             }
             degree++;
         }
